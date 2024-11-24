59bc0972ecc8adec5227be08e2d4d601c7eac8d6
Bharath-PM <bharath.ece.vit@gmail.com>
2024-11-24 11:48:56 +0530

    Update script.js

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ let container=document.querySelector(".container")
 let chatContainer=document.querySelector(".chat-container")
 let btn=document.querySelector(".btn")
 let userMessage=null;
-let Api_url='https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-flash-latest:generateContent?key=AIzaSyC9wIm4pMnQBJY1bdGIcOtjBDKAsd44-zU'
+let Api_url='https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-flash-latest:generateContent?key=YOU-API-KEY'
 
 //const Api_url=paste here your api url , you can watch it on my video
 function createChatBox(html,className){
